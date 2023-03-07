cb59c878b5786613a50ae5c4ec8443df7b4d985c
ThomasFoydel <thomasjfoydel@gmail.com>
2023-03-07 17:21:37 -0500

    initialized spring boot server

---
+++ server/src/main/java/thomasfoydel/brushbrawl/BrushbrawlApplication.java
@@ -0,0 +1,13 @@
+package thomasfoydel.brushbrawl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrushbrawlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrushbrawlApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/thomasfoydel/brushbrawl/BrushbrawlApplicationTests.java
@@ -0,0 +1,13 @@
+package thomasfoydel.brushbrawl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrushbrawlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
