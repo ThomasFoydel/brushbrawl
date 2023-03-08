12e9e234e9684a04810a44193bd498751bc6b35f
ThomasFoydel <thomasjfoydel@gmail.com>
2023-03-08 14:55:03 -0500

    added more dependencies and changed group name

--- server/src/main/java/com/thomasfoydel/brushbrawl/BrushbrawlApplication.java
+++ server/src/main/java/com/thomasfoydel/brushbrawl/BrushbrawlApplication.java
@@ -1,11 +1,10 @@
-package thomasfoydel.brushbrawl;
+package com.thomasfoydel.brushbrawl;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BrushbrawlApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(BrushbrawlApplication.class, args);
 	}
